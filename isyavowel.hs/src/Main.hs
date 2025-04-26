004e83e52adb8d3c7ee9c627cedea98de25fe532
Steffan Sølvsten <201505832@post.au.dk>
2025-04-26 18:13:34 +0200

    Solve 'isyavowel' for Haskell
    This just seemed too fitting for my throwaway joky comment on the
    previous one. But, we learned about qualified imports, and got a
    better feel for list manipulation. There was also some other fights
    with the compiler.

---
+++ isyavowel.hs/src/Main.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+import Control.Monad
+import Data.Function
+import qualified Data.List as List
+
+exists :: (a -> Bool) -> [a] -> Bool
+exists _ []     = False
+exists f (x:xs) = if f x then True else exists f xs
+
+vowels :: Bool -> [Char]
+vowels y = (if y then ['y', 'Y'] else []) ++ ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+count :: (a -> Bool) -> [a] -> Int
+count f xs = map (fromEnum . f) xs & sum
+
+main :: IO ()
+main = do
+  s <- getLine
+  let bools = map (== 1) ([0..1] :: [Int])
+  let preds = map (\ y -> (\ x -> exists (== x) (vowels y))) bools
+  let out   = map (\ f -> count f s) preds
+  putStrLn  $ (map show out) & (List.intersperse " ") & join

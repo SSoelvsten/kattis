f1463f921ebdfedd69e2f8f033a50badb8c80c15
Steffan Sølvsten <201505832@post.au.dk>
2025-04-26 12:40:18 +0200

    Replace do-notation with monadic for 'telja‘
    Just to try and see whether I can get it to work. One could
    even argue this is prettier...?

--- telja.hs/src/Main.hs
+++ telja.hs/src/Main.hs
@@ -1,14 +1,9 @@
 module Main (main) where
 
--- TODO: Switch to IO(Int) and use >> ?
 telja :: Int -> IO ()
-telja x = do
-  if x > 1 then telja (x-1) else return ()
-  print x
+telja x =
+ (if x > 1 then telja (x-1) else return ()) >> (print x)
 
--- TODO: Again, try to use >>=
 main :: IO ()
-main = do
-  x_string <- getLine
-  let x_int = read x_string :: Int
-  telja x_int
+main =
+  getLine >>= (\ x -> telja (read x :: Int))

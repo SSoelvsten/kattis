e1a9e954f26884d92aaa52a9fdbea7e073015588
Steffan Sølvsten <201505832@post.au.dk>
2025-04-26 14:56:40 +0200

    Clean up code with 'getInt' wrapper

--- takkar.hs/src/Main.hs
+++ takkar.hs/src/Main.hs
@@ -1,11 +1,12 @@
 module Main (main) where
 
+getInt :: IO Int
+getInt = getLine >>= (\ x -> return (read x :: Int))
+
 main :: IO ()
 main = do
-  a_str  <- getLine
-  let a = read a_str :: Int
-  b_str <- getLine
-  let b = read b_str :: Int
+  a <- getInt
+  b <- getInt
   putStrLn (if a < b then "FAKE NEWS!"
             else if b < a then "MAGA!"
             else "WORLD WAR 3!")

ec563e5e3c78790b4139febd2be9206aa7a76999
Steffan Sølvsten <201505832@post.au.dk>
2025-05-03 21:17:25 +0200

    Solve ‘euclidsalgorithm' by shortcutting a << b via modulo
    Thanks Wikipedia, I forgot all of these detailg from the algebra course
    I took many years ago

--- euclidsalgorithm.hs/src/Main.hs
+++ euclidsalgorithm.hs/src/Main.hs
@@ -6,9 +6,10 @@ getIntsLine = getLine >>= (return . words) >>= (return . (map read))
 -- NOTE: The Standard library already includes a 'gcd' function
 euclid_gcd :: Int -> Int -> Int
 euclid_gcd a b =
-  if a == b then a
-  else let (a', b') = if a < b then (a, b) else (b, a)
-        in euclid_gcd a' (b' - a')
+  let (a', b') = if a < b then (a, b) else (b, a)
+  in if a' == 0
+     then b'
+     else euclid_gcd a' (b' `mod` a')
 
 main :: IO ()
 main = do
